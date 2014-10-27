0d9437bebbac65637c89fac4753ad804cf9ac512
Dave Mills <dave.a.mills@gmail.com>
2014-10-27 16:57:11 +0000

    version 1.1

--- R/scale_bd.R
+++ R/scale_bd.R
@@ -1,3 +1,14 @@
+#' Transform \code{Date}s into your business-date scale. 
+#' 
+#' @param dates a \code{Date} vector for which you want to transform each date into an
+#'  integer \code{t} which is the number of business days after the first date in
+#'  your \code{business.dates} vector
+#' @param business.dates a vector of \code{Date} objects, sorted ascending
+#' @return An integer vector where each element is the number of business days \code{t} 
+#'  after the first date in your \code{business.dates} vector
+#'  
+#' @export
+#' 
 bd2t <- function(dates, business.dates) {
   result=match(dates, business.dates) - 1
   structure(as.numeric(result), names=names(dates))
@@ -15,50 +26,102 @@ bd_trans <- function(business.dates, breaks=bd_breaks(business.dates)) {
   trans_new('date', transform=transform, inverse=inverse, breaks=breaks, domain=range(business.dates))
 }
 
-scale_bd <- function(aesthetics, expand=waiver(), breaks=bd_breaks(business.dates), minor_breaks=waiver(), business.dates, ...) {
-  
-  if (is.character(breaks)) {
-    breaks_str <- breaks
-    breaks <- date_breaks(breaks_str)
-  }
-  
-  if (is.character(minor_breaks)) {
-    mbreaks_str <- minor_breaks
-    minor_breaks <- date_breaks(mbreaks_str)
-  }
-  
-  continuous_scale(aesthetics, 'date', identity, breaks=breaks, minor_breaks=minor_breaks, guide="none", expand=expand, trans=bd_trans(business.dates, breaks), ...)
+scale_bd <- function(aesthetics, expand=waiver(), breaks, minor_breaks=waiver(), business.dates, ...) {  
+  continuous_scale(aesthetics, 'date', identity, breaks=breaks, minor_breaks=minor_breaks, guide="none", 
+                   expand=expand, trans=bd_trans(business.dates, breaks), ...)
 }
 
-#' Weekend and holiday ignoring position scale for a ggplot
+#' Weekend and holiday ignoring position scale for a ggplot.
 #' 
-#' @param business.dates a vector of Date objects sorted ascending
-#' @param expand see \code{\link{scale_x_date}}
-#' @param breaks see \code{\link{scale_x_date}}
-#' @param minor_breaks see \code{\link{scale_x_date}}
-#' @param ... see \code{\link{scale_x_date}}
+#' @param business.dates a vector of \code{Date} objects, sorted ascending
+#' @param max.major.breaks maximum major breaks \code{\link{bd_breaks}} will return, default=5
+#' @param max.minor.breaks maximum minor breaks \code{\link{bd_breaks}} will return, default=major*5
+#' @param breaks a function \code{max => [date range] => breaks}
+#' @param ... other arguments passed to \code{\link[ggplot2]{continuous_scale}}
 #' 
 #' @export
 #' @import ggplot2 scales
-#' @example exec/example.R
+#' @example exec/example_scale_bd.R
 #' 
-scale_x_bd <- function(..., expand=waiver(), breaks=bd_breaks(business.dates), minor_breaks=waiver(), business.dates) {
-  scale_bd(c("x", "xmin", "xmax", "xend"), expand=expand, breaks=breaks, minor_breaks=minor_breaks, business.dates=business.dates, ...)
+scale_x_bd <- function(..., business.dates, max.major.breaks=5, max.minor.breaks=max.major.breaks*5, breaks=bd_breaks(business.dates)) {
+  
+  scale_bd(c("x", "xmin", "xmax", "xend"), expand=waiver(), 
+           breaks=breaks(max.major.breaks), minor_breaks=breaks(max.minor.breaks), 
+           business.dates=business.dates, ...)
 }
 
-#bd_closest <- function(dates, business.dates) {
-#  m.dates <- do.call(rbind, rep(list(dates), length(business.dates)))
-#  m.bds <- do.call(cbind, rep(list(business.dates), length(dates)))
-#  closest <- abs(m.dates - m.bds) %>% apply(2, function(v) v == min(v))
-#  (closest * m.bds) %>% apply(2, sum) %>% as.Date(origin=as.Date('1970-01-01'))
-#}
+epoch <- as.Date('1970-01-01')
+quarter <- function(date) ceiling(as.integer(format(date, '%m')) / 3)
+quarter_format <- function(date) sprintf("Q%s '%s", quarter(date), format(date, '%y'))
+last_monday <- function(date) as.Date(as.integer(date) - as.integer(format(date, '%u')) + 1, origin=epoch)
 
-bd_breaks <- function(business.dates, n = 5, ...) {
+firstInGroup <- function(dates, f.group) {
+  group <- f.group(dates)
+  grouped <- split(dates, factor(group))
+  result <- sapply(grouped, function(l) l[[1]])
+  as.Date(result, origin=epoch)
+}
+
+#' Date breaks corresponding to the first trading day of standard periods
+#' 
+#' The periods are:
+#' \itemize{
+#'  \item years
+#'  \item quarters
+#'  \item months
+#'  \item weeks
+#'  \item days
+#' }
+#' 
+#' @return returns a function function: \code{max => [date range] => breaks} that generates the breaks 
+#'         for the interval with the largest number of breaks less than \code{n.max}
+#' @param business.dates a vector of \code{Date} objects, sorted ascending
+#' @param n.max the maximum number of breaks to return
+#' 
+#' @export
+#' 
+bd_breaks <- function(business.dates, n.max=5) {
   
-  function(dates) {
-    ts <- bd2t(dates, business.dates)
-    breaks <- t2bd(pretty(sort(ts), n, ...), business.dates)
-    names(breaks) <- attr(breaks, "labels")
-    breaks
-  }
+  breaks.weeks <-firstInGroup(business.dates, last_monday)
+  breaks.months <- firstInGroup(business.dates, function(ds) format(ds, "%b '%y"))
+  breaks.quarters <- firstInGroup(business.dates, quarter_format)
+  breaks.years <- firstInGroup(business.dates, function(ds) format(ds, '%Y'))
+  breaks.years.5 <- firstInGroup(business.dates, function(ds) floor(as.integer(format(ds, '%Y'))/5))
+  breaks.decades <- firstInGroup(business.dates, function(ds) floor(as.integer(format(ds, '%Y'))/10))
+  
+  function(n.max=5) {
+    function(range.date) {      
+      range.t <- bd2t(range.date, business.dates)
+      all.ts <- range.t[1]:range.t[2]
+      all.dates <- t2bd(all.ts, business.dates)
+      
+      if(length(all.dates) <= n.max) return(all.dates)
+      
+      intersection <- function(breaks) as.Date(intersect(all.dates, breaks), origin=epoch)
+      
+      weeks <- intersection(breaks.weeks)
+      
+      if (length(weeks) <= n.max) return (weeks)
+      
+      months <- intersection(breaks.months)
+      
+      if (length(months) <= n.max) return(months)
+      
+      quarters <- intersection(breaks.quarters)
+      
+      if (length(quarters) <= n.max) return(quarters)
+      
+      years <- intersection(breaks.years)
+      
+      if (length(years) <= n.max) return(years)
+      
+      years.5 <- intersection(breaks.years.5)
+    
+      if (length(years.5) <= n.max) return(years.5)
+      
+      decades <- intersection(breaks.decades)
+      
+      decades
+    }  
+  }  
 }
--- README.md
+++ README.md
@@ -1,47 +1,89 @@
 bdscale
 ========
 
-Remove weekends and holidays from `ggplot2` axes.
+Remove Weekends and Holidays From `ggplot2` Axes [![Build Status](https://travis-ci.org/dvmlls/bdscale.svg?branch=master)](https://travis-ci.org/dvmlls/bdscale)
 
-### Get some dates
+### Find valid dates
 
-We need to know which days are valid trading days. Can ask Yahoo Finance for past NYSE trading days:
+Ask Yahoo Finance for S&P prices, use those as past NYSE trading dates. Then create some fake prices, put them into a `data.frame` alongside the dates:
 
 ```
-nyse <- bdscale::get_nyse()
+nyse <- yahoo()
+set.seed(12345)
+df <- data.frame(date=nyse, price=cumsum(rnorm(length(nyse))) + 100)
 ```
 
 ### Plot on standard calendar-day axis:
 
-Create some fake prices, put them into a `data.frame` alongside the dates, and plot:
+Create a plot:
 
 ```
-set.seed(12345)
-df <- data.frame(date=nyse, price=cumsum(rnorm(length(nyse))) + 100)
-
 library(dplyr)
 library(ggplot2)
 
-df %>% filter(as.Date('2014-08-01') < date & date < as.Date('2014-10-08')) %>% 
+plot <- df %>% filter(as.Date('2014-08-01') <= date & date <= as.Date('2014-10-08')) %>% 
   ggplot(aes(x=date, y=price)) + geom_step() + 
-  ggtitle('calendar dates')
+  theme(axis.title.x=element_blank(), axis.title.y=element_blank())
+  
+plot + ggtitle('calendar dates')
 
 ```
 
-This yields a graph with a large gap at the beginning of September, because Labor Day was on the 1st:
+Note the large gap at the beginning of September, because Labor Day was on the 1st:
 
 <img src='man/figures/calendar.PNG'>
 
 ### Plot on a business-day axis:
 
-If instead you plot against `scale_x_bd`:
+Plot against `scale_x_bd` instead:
 
 ```
-df %>% filter(as.Date('2014-08-01') < date & date < as.Date('2014-10-08')) %>% 
-  ggplot(aes(x=date, y=price)) + geom_step() + bdscale::scale_x_bd(business.dates=nyse) +
-  ggtitle('business dates')
+plot + scale_x_bd(business.dates=nyse, labels=date_format("%b '%y")) + 
+  ggtitle('business dates, month breaks')
 ```
 
-You'll remove weekends and holidays from the graph:
+Removes weekends and holidays from the graph:
 
-<img src='man/figures/business.PNG'>
+<img src='man/figures/business.month.PNG'>
+
+The major breaks are pretty far apart, on the first trading day of each month. 
+
+It's a wide chart, tell it to use more breaks:
+
+```
+plot + scale_x_bd(business.dates=nyse, max.major.breaks=10, labels=date_format('%b %d')) + 
+  ggtitle('business dates, week breaks')
+```
+
+Given that max, it determines it can put major breaks on the first trading day of the week, and minor breaks every day:
+
+<img src='man/figures/business.week.PNG'>
+
+### Translating into business-day space
+
+Say I wanted to put vertical lines on option expiration dates. 
+
+Calling `as.numeric` on my dates translates them into the the number of calendar days after the unix epoch, which is what `scale_x_date` uses (see `scales:::from_date`):
+
+```
+options <- c('2014-08-15', '2014-09-19') %>% as.Date
+
+plot + 
+  geom_vline(xintercept=as.numeric(options), size=2, alpha=0.25) + 
+  ggtitle('calendar dates, option expiry')
+````
+
+<img src='man/figures/calendar.options.PNG'>
+
+This doesn't work for business-day space because the x-axis now represents the number of business days after the first date in your `business.dates` vector. 
+
+Instead, use the `bdscale::bd2t` function to translate into business-day space:
+
+```
+plot + 
+  geom_vline(xintercept=bd2t(options, business.dates=nyse), size=2, alpha=0.25) + 
+  scale_x_bd(business.dates=nyse) +
+  ggtitle('business dates, option expiry')
+```
+
+<img src='man/figures/business.options.PNG'>
